941bac4a728c88c1f7f96211b576200bed87c726
Jeff Baranski <jeff.baranski@outlook.com>
2024-12-23 19:09:34 -0500

    client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,5 +8,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  teams = [
+    'Atlanta United FC',
+    'Austin',
+    'CF Montreal',
+    'Charlotte',
+    'Chicago Fire',
+    'Colorado Rapids',
+    'Columbus Crew',
+    'DC United',
+    'FC Cincinnati',
+    'FC Dallas',
+    'Houston Dynamo',
+    'Inter Miami',
+    'Los Angeles FC',
+    'Los Angeles Galaxy',
+    'Minnesota United FC',
+    'Nashville SC',
+    'New England Revolution',
+    'New York City FC',
+    'New York Red Bulls',
+    'Orlando City SC',
+    'Philadelphia Union',
+    'Portland Timbers',
+    'Real Salt Lake',
+    'San Diego',
+    'San Jose Earthquakes',
+    'Seattle Sounders',
+    'Sporting Kansas City',
+    'St. Louis City',
+    'Toronto FC',
+    'Vancouver Whitecaps'
+  ];
 }
